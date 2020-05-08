e98ddc1f177677cd12119f3dd096dce92c7ba123
Maxim Antonuk <maxim.antonuk@gmail.com>
2020-05-09 00:56:52 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import { generate as id } from "shortid";
 
 import {AppContext} from './AppContext'
 
-console.log(AppContext);
-
 class App extends Component {
     state = {
         items : defaultState,
